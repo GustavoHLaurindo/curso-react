fb65f1682442399ca79b6203eb048c97f9eefc74
Gustavo Henrique <gustavohenriquelaurindo22@gmail.com>
2023-05-08 21:12:08 -0300

    Iniciando Projeto
    criei a base do projeto

---
+++ desafio-jogo-silvio-santos/secretword/src/components/StartScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './StartScreen.css'
+const StartScreen = () => {
+  return (
+        <div className="root">
+            <div className="container">
+                <h1 className="small title">Secret</h1>
+                <h1 className="big title">Word</h1>
+                <button>Play!</button>
+            </div>
+      </div>
+)}
+
+export default StartScreen
